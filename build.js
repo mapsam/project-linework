bc7f25f01f3aa5a126d494f66a1c90d2869351b7
mapsam <matthews.sam@gmail.com>
2015-07-23 13:02:36 -0700

    add to s3 bucket

--- build.js
+++ build.js
@@ -1,11 +1,21 @@
 var config = require('./config'),
     archiver = require('archiver');
-    fs = require('fs');
+    fs = require('fs'),
+    s3 = require('s3');
+
+var client = s3.createClient({
+  maxAsyncS3: 20,
+  s3RetryCount: 3,
+  s3RetryDelay: 1000,
+  multipartUploadThreshold: 20971520,
+  multipartUploadSize: 15728640,
+  s3Options: {
+    accessKeyId: config.accessKeyId,
+    secretAccessKey: config.accessKeySecret,
+  },
+});
 
 
-
-
-// console.log(config.waka);
 
 fs.readdir('./linework-sets', callback);
 
@@ -16,32 +26,58 @@ function callback(err, data) {
     (function(object) {
 
       var path = './linework-sets/' + object;
+      var name = object;
       if (fs.lstatSync(path).isDirectory()) {
         var output = fs.createWriteStream(path + '.zip');
         var archive = archiver('zip');
+        archive.pipe(output);
 
-        output.on('close', function() {
-          console.log(archive.pointer() + ' total bytes');
-          console.log('archiver has been finalized and the output file descriptor has closed.');
+        output.on('close', function(){
+          upload(this, object);
         });
-
         archive.on('error', function(err) {
           throw err;
         });
-
-        archive.pipe(output);
 
         archive.bulk([
           { expand: true, cwd: path, src: ['**/*.*'] }
         ]);
 
         archive.finalize();
+
       }
 
     }(data[i]));
   }
 }
 
+
+function upload(object, name) {
+
+  // console.log(path);
+
+  var params = {
+    localFile: object.path,
+
+    s3Params: {
+      Bucket: 'giscollective',
+      Key: 'projectlinework/' + name + '.zip',
+      ACL: 'public-read'
+    },
+  };
+  var uploader = client.uploadFile(params);
+  uploader.on('error', function(err) {
+    console.error("unable to upload:", err.stack);
+  });
+  uploader.on('progress', function() {
+    console.log("progress", uploader.progressMd5Amount,
+              uploader.progressAmount, uploader.progressTotal);
+  });
+  uploader.on('end', function() {
+    console.log("done uploading");
+  });
+
+}
 
 
 
